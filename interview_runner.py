26bfa707e2fa4269325151cac079fd0ad8d7c2e0
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-10-02 18:35:00 +0900

    chore: [최민지] 필요없는 주석 제거 [AIMSAC-27]

--- interview_runner.py
+++ interview_runner.py
@@ -1,7 +1,8 @@
 import glob
 import time
 import os
-
+import json
+import numpy as np
 from tqdm import tqdm
 
 from interview_preprocessing.service.interview_preprocessing_service_impl import InterviewPreprocessingServiceImpl
@@ -98,72 +99,57 @@ def getKeyword(inputFilePath):
     interview.saveFile(interviewList, savePath)
 
 
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-import json
-
-# 코사인 유사도 계산 함수
-
-# 유사도 비교 함수
 def match_vecs():
     sequence = ['협업_능력', '대처_능력', '적응력', '기술적_역량', '프로젝트_경험']
     startAnswerList = interview.readFile(f'assets\\json_data_embedding\\{sequence[0]}_embedded.json')
 
-    # 결과 저장을 위한 카운터 초기화
     cnt = 0
 
-    # 협업 능력 파일 안의 각 answer_vec에 대해 처리
     for startAnswer in tqdm(startAnswerList, total=len(startAnswerList), desc='match_vecs'):
-        # 매칭 결과를 저장할 리스트
         matched_data = []
-        used_question_vecs = set()  # 사용된 question_vec을 저장할 집합
+        used_question_vecs = set()
 
-        # 협업 능력 정보 먼저 저장
-        currentAnswerVec = tuple(startAnswer.get('answer_vec'))  # 협업_능력 파일의 answer_vec으로 시작
+        currentAnswerVec = tuple(startAnswer.get('answer_vec'))
         matched_data.append({
-            "question": startAnswer['question'],  # 협업 능력 질문
-            "answer": startAnswer["answer"],  # 협업 능력 답변
-            "occupation": startAnswer['occupation'],  # 직업
-            "experience": startAnswer.get('experience', 'NEW'),  # 경험
-            "intent": startAnswer['rule_based_intent'],  # 의도
+            "question": startAnswer['question'],
+            "answer": startAnswer["answer"],
+            "occupation": startAnswer['occupation'],
+            "experience": startAnswer.get('experience', 'NEW'),
+            "intent": startAnswer['rule_based_intent'],
         })
 
-        # 이후 대처 능력, 적응력, 기술적 역량, 프로젝트 경험 순으로 비교
         for i in range(1, len(sequence)):
             nextQuestionList = interview.readFile(f'assets\\json_data_embedding\\{sequence[i]}_embedded.json')
             highest_similarity = -1
             best_match = None
 
-            # 현재 answer_vec와 비교할 다음 파일의 question_vec들
             for nextQuestion in nextQuestionList:
                 questionVec = tuple(nextQuestion.get('question_vec'))
 
                 if nextQuestion.get('question') in used_question_vecs:
-                    continue  # 이미 사용된 질문은 비교에서 제외
+                    continue
 
                 similarity = interview.cosineSimilarityBySentenceTransformer(
                     np.array(currentAnswerVec).reshape(1, -1),
                     np.array(questionVec).reshape(1, -1)
                 )
 
-                # 유사도가 가장 높은 question_vec 선택
                 if similarity > highest_similarity:
                     highest_similarity = similarity
                     best_match = {
-                        "question": nextQuestion['question'],  # 유사한 질문
-                        "answer": nextQuestion.get('answer'),  # 해당 질문의 답변
-                        "occupation": nextQuestion.get('occupation'),  # 직업
-                        "experience": nextQuestion.get('experience'),  # 경험
-                        "intent": nextQuestion.get('rule_based_intent'),  # 의도
+                        "question": nextQuestion['question'],
+                        "answer": nextQuestion.get('answer'),
+                        "occupation": nextQuestion.get('occupation'),
+                        "experience": nextQuestion.get('experience'),
+                        "intent": nextQuestion.get('rule_based_intent'),
                         "similarity": highest_similarity.tolist(),
                         "answer_vec": nextQuestion.get('answer_vec')
                     }
 
-                # 가장 유사한 질문을 찾았다면 matched_data에 추가하고, 다음 비교를 위해 answer_vec 갱신
             if best_match:
                 matched_data.append(best_match)
                 used_question_vecs.add(best_match["question"])
-                currentAnswerVec = tuple(best_match.get('answer_vec'))  # 다음 비교를 위한 answer_vec 업데이트
+                currentAnswerVec = tuple(best_match.get('answer_vec'))
         print(matched_data)
 
         cnt += 1
