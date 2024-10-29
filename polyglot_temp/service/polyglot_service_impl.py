8888767c9d8fb16e39e8f1a569883c88b36ab6cf
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-10-29 12:25:59 +0900

    fix: [최민지] 비동기 처리가 가능한 병렬 작업으로 수정 [AIMSAC-44]

--- polyglot_temp/service/polyglot_service_impl.py
+++ polyglot_temp/service/polyglot_service_impl.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 from polyglot_temp.repository.polyglot_repository_impl import PolyglotRepositoryImpl
 from polyglot_temp.service.polyglot_service import PolyglotService
+from template.utility.color_print import ColorPrinter
 
 
 class PolyglotServiceImpl(PolyglotService):
@@ -37,24 +38,13 @@ class PolyglotServiceImpl(PolyglotService):
             self.__polyglotRepository.downloadPretrainedModel()
 
         interviewList = arg
-        print("interviewList: ", interviewList)
-
-        resultList = []
 
         # 각 인터뷰에 대해 비동기 작업 생성
         tasks = [
             self.__polyglotRepository.scoreUserAnswer(interview[0], interview[1], interview[2])
             for interview in interviewList
         ]
-
         # 모든 비동기 작업을 병렬로 실행
-        results = await asyncio.gather(*tasks)
-        print('results: ', results)
-        # resultList = []
-        # for interview in interviewList:
-        #     question, userAnswer, intent = interview[0], interview[1], interview[2]
-        #     print(f"question: {question}\nuserAnswer: {userAnswer}\nintent: {intent}")
-        #     result = await self.__polyglotRepository.scoreUserAnswer(question, userAnswer, intent)
-        #     resultList.append(result)
-
-        return {'resultList': results}
+        resultList = await asyncio.gather(*tasks)
+        ColorPrinter.print_important_message(f'resultList: {resultList}')
+        return {'resultList': resultList}
